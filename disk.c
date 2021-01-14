19537a03d983da8948841e04266285c78ae958b1
Bara C Tudor <vector2025@gmail.com>
2021-01-15 01:55:03 +0200

    acum folosim fork + bug-ul cu permission denied

--- disk.c
+++ disk.c
@@ -15,6 +15,7 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <stdarg.h>
+#include <pwd.h>
 
 #define CHECK(...) if((__VA_ARGS__) < 0) report_and_exit(#__VA_ARGS__);
 
@@ -26,12 +27,13 @@ void report_and_exit(const char* msg){
 typedef struct process_info_s {
     int proc_id;        // id-ul din sistem
     char path[128];     // path de analizat
-    char filename[32];  // file asociat cu task-ul in care se scrie process_data_t
+    char filename[128];  // file asociat cu task-ul in care se scrie process_data_t
 } process_info_t;
 
 
 // in 2 locuti mai trebuie updatat daca se schimba dir-ul: read_proc_list si set_task_filenam
-static const char* PROC_LIST_FILENAME = "~/.da_cache_d/.proc_list";
+static const char* s_proc_folder = ".da_cache_d/";
+static const char* PROC_LIST_FILENAME = ".da_cache_d/proc_list";
 //static const char* PROC_LIST_FILENAME = "/tmp/da_proc_list";
 static int s_fd; // fd pentru PROC_LIST_FILENAME
 static int s_proc_num; // number of processes
@@ -49,6 +51,7 @@ typedef struct process_data_s {
     int progress; //  0 la 100
     enum status status; // pending | in progress | done
     int files;
+    long long total_size;
     int dirs;
     int line_num; // numarul de linii pe care il contine mesajul afisat prin --print <id>
 } process_data_t;
@@ -60,6 +63,8 @@ int search_folder(
     struct process_data_s* process_data,
     int is_root_folder
 );
+long long print_search_folder_header(const char* path);
+long long count_path_size(const char* local_path);
 
 char* str_status(enum status status){
     switch(status){
@@ -84,14 +89,32 @@ char* str_priority(int prio){
     }
 }
 
+char s_priv_fmt_buff[20];
+char* str_format_size(int size){
+    return s_priv_fmt_buff;
+}
+
 // populeaza s_proc_ids
 void read_proc_info_list(){
     char path[PATH_MAX];
-    realpath("~/.da_cache_d", path);
-    mkdir(path, S_IRUSR | S_IWUSR); // nu-l face de doua ori, daca exista atunci ret==-1
+    /* struct passwd* pwd = getpwuid(getuid()); */
+    /* strcpy(path, pwd->pw_dir); */
+    /* strcat(path, "/.da_cache_d/"); */
 
-    realpath(PROC_LIST_FILENAME, path);
-    CHECK(s_fd = open(path, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR));
+    /* mkdir(path, 0666); */
+    /* strcpy(s_proc_folder, path); */
+    /* strcat(path, PROC_LIST_FILENAME); */
+
+    //realpath("~/", path);
+    //printf("realpath: %s", );
+    //realpath("~/.da_cache_d", path);
+    //mkdir(path, S_IRUSR | S_IWUSR);
+    struct stat st;
+    if(stat(s_proc_folder, &st) == -1)
+        mkdir(s_proc_folder, 0777);
+    //realpath(PROC_LIST_FILENAME, path);
+    //CHECK(s_fd = open(path, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR));
+    CHECK(s_fd = open(PROC_LIST_FILENAME, O_RDWR | O_CREAT));
     int read_bytes;
     CHECK(read_bytes = read(s_fd, &s_proc_num, sizeof(s_proc_num)));
     if(read_bytes == 0){
@@ -105,8 +128,11 @@ void read_proc_info_list(){
 }
 
 void write_proc_info_list(){
-    if(s_proc_num == 0)
+    if(s_proc_num == 0){
+        close(s_fd);
+        close(open(PROC_LIST_FILENAME, O_WRONLY | O_TRUNC));
         return;
+    }
     lseek(s_fd, 0, SEEK_SET);
     CHECK(write(s_fd, &s_proc_num, sizeof(s_proc_num)));
     for(int i = 0; i < s_proc_num; i++){
@@ -123,9 +149,11 @@ process_info_t* find_process_info(int id){
 }
 
 void set_task_filename(char* filename, int id){
-    const char* prefix = "~/.da_cache_d/.task";
+    const char* prefix = ".da_cache_d/task";
+    //const char* prefix = "~/.da_cache_d/.task";
     strcpy(filename, prefix);
-    sprintf(filename + strlen(prefix), "%d", id);
+    /* strcat(filename, ".task"); */
+    sprintf(filename + strlen(filename), "%d", id);
 }
 
 // flock folosit pentru sincronizarea dintre procesul task si procesul main care interogheaza
@@ -156,13 +184,19 @@ void write_proc_data(char* filename, const process_data_t* data){
 }
 
 void proc_add(const char* p, int priority){
-
     char *path=realpath(p, NULL);
-    if (path == NULL)
-    {
-    	printf("Nu exista fisierul\n");
+    if (path == NULL) {
+    	printf("Folder does not exist\n");
     	return;
     }
+    struct stat st;
+
+    CHECK(stat(path, &st));
+    if(!S_ISDIR(st.st_mode)){
+        printf("Not a folder\n");
+        return;
+    }
+
     read_proc_info_list();
 
     // mai intai vedem daca exista un task pentru acest path
@@ -182,22 +216,21 @@ void proc_add(const char* p, int priority){
         set_task_filename(proc_data.info.filename, getpid());
         strcpy(proc_data.info.path, path);
         proc_data.priority = priority;
-        proc_data.progress = STATUS_PROGRESS;
-        proc_data.status = 0;
-        proc_data.files = 0;
-        proc_data.dirs = 0;
+        proc_data.status = STATUS_PROGRESS;
         setpriority(PRIO_PROCESS, proc_data.info.proc_id, proc_data.priority);
         int fd;
-        CHECK(fd = creat(proc_data.info.filename, S_IRUSR | S_IWUSR));
+        CHECK(fd = creat(proc_data.info.filename, 0666));
         CHECK(close(fd));
-        printf("Created analysis task with ID '%d', for '%s' and priority %s", proc_data.info.proc_id, proc_data.info.path, str_priority(proc_data.priority));
+        printf("Created analysis task with ID '%d', for '%s' and priority %s\n", proc_data.info.proc_id, proc_data.info.path, str_priority(proc_data.priority));
         write_proc_data(proc_data.info.filename, &proc_data);
         free(path);
-        // TODO apel functie de analiza
-        // struct process_data_s data;
-        // long long global_folder_size;
-        // global_folder_size = print_search_folder_header("test");
-        // search_folder("test","",global_folder_size,&data,1);
+
+        long long global_folder_size = count_path_size(proc_data.info.path);
+        proc_data.total_size = global_folder_size;
+        write_proc_data(proc_data.info.filename, &proc_data);
+        search_folder(proc_data.info.path, "", global_folder_size, &proc_data, 1);
+        proc_data.status = STATUS_DONE;
+        write_proc_data(proc_data.info.filename, &proc_data);
     }
     else { // parent/main
         s_proc_list[s_proc_num].proc_id = pid;
@@ -227,7 +260,7 @@ void proc_suspend(int id){
         CHECK(write(fd, &data, sizeof(data)));
         break;
     case STATUS_DONE:
-        printf("Task is done no need to suspend for '%s'", info->path);
+        printf("Task is done no need to suspend for '%s'\n", info->path);
         break;
     }
     close_and_unlock(fd);
@@ -263,8 +296,6 @@ void proc_remove(int id){
     process_info_t* info = find_process_info(id);
     process_data_t data;
     int fd = read_proc_data_lock(info->filename, &data);
-    /* int fd = open(info->filename, O_RDONLY); */
-    /* CHECK(read(fd, &data, sizeof(data))); */
 
     switch(data.status)
     {
@@ -281,7 +312,7 @@ void proc_remove(int id){
                 break;
             }
         }
-        for(int i = index; i < s_proc_num - 1; i++){
+        for(int i = index; i < s_proc_num; i++){
             s_proc_list[i].proc_id = s_proc_list[i++].proc_id;
             strcpy(s_proc_list[i].path, s_proc_list[i++].path);
             strcpy(s_proc_list[i].filename, s_proc_list[i++].filename);
@@ -299,6 +330,142 @@ void proc_info(int id){
     read_proc_data(info->filename, &data);
     printf("Status '%s' for '%s'\n", str_status(data.status), data.info.path);
 }
+
+
+/* void proc_print_recursively(int i, int limit, char** buff, size_t* len, FILE* fp){ */
+/*     if(i<limit) */
+/*         proc_print_recursively(i+1,limit,buff,len,fp); */
+/*     CHECK(getdelim(&buff, &len, '\n', fp)); */
+/*         //fgets(buff, sizeof(buff)); */
+/*     if(getline(buff, len, fp) > 0) */
+/*         printf("%s", *buff); */
+/* } */
+
+void proc_print(int id){
+    read_proc_info_list();
+    process_info_t* info = find_process_info(id);
+
+    process_data_t data;
+    int fd = read_proc_data_lock(info->filename, &data);
+    printf("Path    Usage   Size    Amount\n");
+    printf("%s 100%% %lld ", data.info.path, data.total_size);
+    for(int i=0; i<50; i++){
+        printf("#");
+    }
+    printf("|\n");
+    if(data.status == STATUS_DONE){
+        char* buff = NULL;
+        size_t len = 0;
+        FILE* fp = fdopen(fd, "r");
+        for(int i = 0; i < data.line_num; i++){
+            if(getline(&buff, &len, fp) > 0){
+                printf("%s", buff);
+            }
+        }
+        //proc_print_recursively(0,data.line_num,&buff,&len,fp);
+    }
+    close_and_unlock(fd);
+}
+
+void proc_list(){
+    read_proc_info_list();
+    if(s_proc_num == 0){
+        printf("No analysis found\n");
+        return;
+    }
+    printf( "%-7s %-3s %-4s %-15s %-s %-30s\n", "ID", "PRI", "Done" , "Status", "Details", "Path");
+    for(int i=0; i<s_proc_num; i++){
+        process_info_t* info = &s_proc_list[i];
+        process_data_t data;
+    	read_proc_data(info->filename, &data);
+        char prio[4];
+        memset(prio, 0, 4);
+        for(int i=0; i<data.priority; i++)
+            prio[i] = '*';
+        printf("%-7d %-3s %-d%% %-10s %-3d files %-2d dirs %-30s\n",info->proc_id, prio, data.progress, str_status(data.status), data.files, data.dirs, info->path);
+    }
+}
+
+// pt FLORIAN: folosesti functia asta in loc de printf
+// IMPORTANT, cand o apelezi sa scrii un rand intreg, adica sa aiba fmt-ul un '\n'; desi din cate vad deja faci asta
+void write_proc_data_message(process_data_t* data, char* fmt, ...){
+    va_list args;
+    va_start(args, fmt);
+    data->line_num++;
+    write_proc_data(data->info.filename, data);
+
+    int fd = open(data->info.filename, O_WRONLY | O_APPEND);
+    CHECK(flock(fd, LOCK_EX));
+    FILE* fp = fdopen(fd, "a");
+    vfprintf(fp, fmt, args);
+    CHECK(flock(fd, LOCK_UN));
+    CHECK(fclose(fp));
+    va_end(args);
+}
+
+/// Cauta recursiv pornind de la 'global_path'
+int search_folder(const char* local_path,
+    const char* dirname,
+    long long global_folder_size,
+    struct process_data_s *process_data,
+    int is_root_folder
+){
+
+    struct dirent *dent;
+    DIR* srcdir = opendir(local_path);
+    if(!srcdir){
+        perror("opendir");
+        return -1;
+    }
+    int total_file_size = 0;
+    char* relative_path = malloc(256);
+    // char* next_folder = malloc(256);
+    while((dent = readdir(srcdir)) != 0){
+        memset(relative_path,0,256);
+        // memset(next_folder,0,256);
+        struct stat dir_stat;
+
+        if((strcmp(dent->d_name,".") == 0) || (strcmp(dent->d_name, "..") == 0))
+            continue;
+
+        // Folosim 'fstatat' in loc de 'stat' deoarece avem de a face cu folder
+        if(fstatat(dirfd(srcdir), dent->d_name, &dir_stat,0) < 0){
+            perror(dent->d_name);
+            continue;
+        }
+        strcpy(relative_path, local_path);
+        strcat(relative_path,"/");
+        strcat(relative_path,dent->d_name);
+        if(S_ISDIR(dir_stat.st_mode)){
+            // strcpy(next_folder,local_path);
+            // strcat(next_folder,"/");
+            // strcat(next_folder,dent->d_name);
+            int this_folder_size = search_folder(relative_path,dent->d_name,global_folder_size,process_data,is_root_folder+1);
+            process_data->dirs++;
+            float current_percentage = ((float)this_folder_size/(float)global_folder_size)*100;
+            //write_proc_data_message();
+            char* hashtags = calloc(52,1);
+            for(int i = 0; i< (int)current_percentage/2+1; i++)
+                hashtags[i] = '#';
+            //printf("|-/%s %.2f%% %d %s\n",relative_path,current_percentage ,this_folder_size, hashtags);
+            write_proc_data_message(process_data,"|-/%s %.2f%% %d \n",relative_path,current_percentage ,this_folder_size);
+            free(hashtags);
+        }
+        else{
+            total_file_size += dir_stat.st_size;
+        //incrementam numarul de fisiere
+            // long int current_percentage = (dir_stat.st_size/prev_folder_size)*100;
+            // printf("FILE : |-/%s %lld%% %ld \n",relative_path, current_percentage ,dir_stat.st_size);
+            process_data->progress += dir_stat.st_size;
+            process_data->files++;
+        }
+    }
+    free(relative_path);
+    // free(next_folder);
+    closedir(srcdir);
+    return total_file_size;
+}
+
 
 long long count_path_size(const char* local_path){
     struct dirent *dent;
@@ -336,159 +503,6 @@ long long count_path_size(const char* local_path){
     // free(next_folder);
     closedir(srcdir);
     return this_dir_size;
-}
-
-// Afiseaza headerul procesului si returneaza dimensiunea folderului principal
-long long print_search_folder_header(const char* path){
-    struct dirent *dent;
-    DIR* srcdir = opendir(path);
-    struct stat global_dir_stat; // info despre primul folder
-    char* absolute_path= realpath(path,NULL);
-    long long folder_size = count_path_size(absolute_path);
-    //afisam prima linie: "Path   Usage   Size   Amount"
-    if((dent = readdir(srcdir)) != 0){
-        printf("Path    Usage   Size    Amount\n");
-        printf("%s 100%% %lld\n",absolute_path,folder_size);
-        printf("|\n");
-    }
-    free(absolute_path);
-    closedir(srcdir);
-    return folder_size;
-}
-
-void proc_print_recursively(int i, int limit, char** buff, size_t* len, FILE* fp){
-    if(i<limit)
-        proc_print_recursively(i+1,limit,buff,len,fp);
-    CHECK(getdelim(&buff, &len, '\n', fp));
-        //fgets(buff, sizeof(buff));
-    if(getline(buff, len, fp) > 0)
-        printf("%s", *buff);
-}
-
-void proc_print(int id){
-    /// DE MUTAT IN ADD
-    struct process_data_s data;
-    long long global_folder_size;
-    global_folder_size = print_search_folder_header("test"); // afiseaza header-ul cautarii, trebuie inlocuit
-    search_folder("test","",global_folder_size,&data,1);
-
-    read_proc_info_list();
-    process_info_t* info = find_process_info(id);
-
-    process_data_t data;
-    int fd = read_proc_data_lock(info->filename, &data);
-    if(data.status == STATUS_DONE){
-        char buff[1024];
-        size_t len = 1024;
-        FILE* fp = fdopen(fd, "r");
-        // for(int i = 0; i < data.line_num; i++){
-        //     //CHECK(getdelim(&buff, &len, '\n', fp));
-        //     //fgets(buff, sizeof(buff));
-        //     if(getline(&buff, &len, fp) > 0)
-        //         printf("%s", buff);
-        // }
-        proc_print_recursively(0,data.line_num,&buff,&len,fp);
-    }
-    close_and_unlock(fd);
-}
-
-void proc_list(){
-    read_proc_info_list();
-    printf( "%-7s %-3s %-4s %-15s %-s %-30s\n", "ID", "PRI", "Done" , "Status", "Details", "Path");
-    for(int i=0; i<s_proc_num; i++){
-        process_info_t* info = &s_proc_list[i];
-        process_data_t data;
-    	read_proc_data(info->filename, &data);
-        printf("%-7d %-3s %-d%% %-10s %-3d files %-2d dirs %-30s\n",info->proc_id, str_priority(data.priority), data.progress,str_status(data.status), data.files, data.dirs, info->path);
-    }
-}
-
-// pt FLORIAN: folosesti functia asta in loc de printf
-// IMPORTANT, cand o apelezi sa scrii un rand intreg, adica sa aiba fmt-ul un '\n'; desi din cate vad deja faci asta
-void write_proc_data_message(process_data_t* data, char* fmt, ...){
-    va_list args;
-    va_start(args, fmt);
-    write_proc_data(data->info.filename, data);
-    int fd = open(data->info.filename, O_WRONLY | O_APPEND);
-    CHECK(flock(fd, LOCK_EX));
-    FILE* fp = fdopen(fd, "a");
-    vfprintf(fp, fmt, args);
-    CHECK(flock(fd, LOCK_UN));
-    CHECK(fclose(fp));
-    //CHECK(close(fd));
-    va_end(args);
-    data->line_num++;
-}
-
-/// Cauta recursiv pornind de la 'global_path'
-int search_folder(const char* local_path,
-    const char* dirname,
-    long long global_folder_size,
-    struct process_data_s *process_data,
-    int is_root_folder
-){
-
-    struct dirent *dent;
-    DIR* srcdir = opendir(local_path);
-    if(!srcdir){
-        perror("opendir");
-        return -1;
-    }
-    int total_file_size = 0;
-    char* relative_path = malloc(256);
-    // char* next_folder = malloc(256);
-    while((dent = readdir(srcdir)) != 0){
-        memset(relative_path,0,256);
-        // memset(next_folder,0,256);
-        struct stat dir_stat;
-
-        if((strcmp(dent->d_name,".") == 0) || (strcmp(dent->d_name, "..") == 0))
-            continue;
-
-        // Folosim 'fstatat' in loc de 'stat' deoarece avem de a face cu folder
-        if(fstatat(dirfd(srcdir), dent->d_name, &dir_stat,0) < 0){
-            perror(dent->d_name);
-            continue;
-        }
-        if(is_root_folder > 1){
-
-            strcpy(relative_path, local_path);
-            strcat(relative_path,"/");
-        }
-        strcat(relative_path,dent->d_name);
-        if(S_ISDIR(dir_stat.st_mode)){
-            // strcpy(next_folder,local_path);
-            // strcat(next_folder,"/");
-            // strcat(next_folder,dent->d_name);
-            int this_folder_size = search_folder(relative_path,dent->d_name,global_folder_size,process_data,is_root_folder+1);
-            process_data->dirs++;
-            float current_percentage = ((float)this_folder_size/(float)global_folder_size)*100;
-            //write_proc_data_message();
-            char* hashtags = calloc(52,1);
-            for(int i = 0; i< (int)current_percentage/2+1; i++)
-                hashtags[i] = '#';
-            printf("|-/%s %.2f%% %d %s\n",relative_path,current_percentage ,this_folder_size, hashtags);            // write_proc_data_message(process_data,"|-/%s %.2f%% %d \n",relative_path,current_percentage ,this_folder_size);
-            free(hashtags);
-        }
-        else{
-            total_file_size += dir_stat.st_size;
-        //incrementam numarul de fisiere
-            // long int current_percentage = (dir_stat.st_size/prev_folder_size)*100;
-            // printf("FILE : |-/%s %lld%% %ld \n",relative_path, current_percentage ,dir_stat.st_size);
-            process_data->progress += dir_stat.st_size;
-            process_data->files++;
-        }
-    }
-    free(relative_path);
-    // free(next_folder);
-    closedir(srcdir);
-    return total_file_size;
-}
-
-void check_file(const char* path){
-    struct stat stats;
-    if(!stat(path, &stats))
-        printf("The size of the folder is: %ld \n", stats.st_size);
 }
 
 
